cca4c6ef722bc4600c431079694ae85d06fbfea3
Poltarakov <epoltarakov.99@mail.ru>
2022-05-20 00:28:15 +0300

    Create aop and aspects packeges, create Config class with annotatios. Create LoggingAspect class with annotation @Aspect and @Before, advise method and pointcut.

---
+++ src/aop/Library.java
@@ -0,0 +1,12 @@
+package aop;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component("libraryBean")
+public class Library {
+
+    public void getBook(){
+        System.out.println("Мы берем книгу");
+    }
+}
