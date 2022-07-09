a5142de88d7decd9ad96003ab4fdab6e3529218c
Poltarakov <epoltarakov.99@mail.ru>
2022-07-09 20:24:06 +0300

    Learn One-To-One uni-directionaly in tests

--- src/hibernate_test2/entity/Test1.java
+++ src/hibernate_test2/entity/Test1.java
@@ -1,6 +1,6 @@
 package hibernate_test2.entity;
 
-import hibernate_test.entity.Employee;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -9,22 +9,81 @@ public class Test1 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
-                .addAnnotatedClass(hibernate_test.entity.Employee.class)
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
                 .buildSessionFactory();
 
 
-        try {
-            Session session = factory.getCurrentSession();
-            hibernate_test.entity.Employee emp = new Employee("Aleksandr", "Petrov",
-                    "IT", 600);
-            session.beginTransaction();
-            session.save(emp);
-            session.getTransaction().commit();
+//        try {
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Egor", "Poltarakov"
+//                    , "IT", 500);
+//            Detail detail = new Detail("SPB", "123456789", "egorpoltarakov@gmail.com");
+//
+//            employee.setEmpDetail(detail);
+//            session.beginTransaction();
+//
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+//
+//        }
+//        finally {
+//            factory.close();
+//        }
 
+
+//        try {
+//            Session session = factory.getCurrentSession();
+//            Employee employee = new Employee("Oleg", "Smirnov"
+//                    , "Sales", 700);
+//            Detail detail = new Detail("Moscow", "987456123", "olejka@gmail.com");
+//
+//            employee.setEmpDetail(detail);
+//            session.beginTransaction();
+//
+//            session.save(employee);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+//
+//        }
+//        finally {
+//            factory.close();
+//        }
+
+//        Session session = null;
+//        try {
+//            session = factory.getCurrentSession();
+//            session.beginTransaction();
+//
+//            Employee emp = session.get(Employee.class, 10);
+//            System.out.println(emp.getEmpDetail());
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
+//
+//        }
+//        finally {
+//            session.close();
+//            factory.close();
+//        }
+
+        Session session = null;
+        try {
+            session = factory.getCurrentSession();
+            session.beginTransaction();
+
+            Employee emp = session.get(Employee.class, 2);
+            session.delete(emp);
+
+            session.getTransaction().commit();
             System.out.println("Done!");
-            System.out.println(emp);
+
         }
         finally {
+            session.close();
             factory.close();
         }
     }
